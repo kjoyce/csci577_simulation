3cd433119da3ef12e48e5ed6dc5a382490ef93f1
Patrick Funk <pfunkafide@gmail.com>
2013-01-31 18:22:38 -0700

    Added Eulers Method

--- lab1_coffee/coffee_problem.py
+++ lab1_coffee/coffee_problem.py
@@ -17,5 +17,17 @@ c_cream = 1/dot(time.T,time) * dot(-time.T,tcream)
 
 def actual_T(t,c,T0):
   return (T0 - 23)*exp(-c*t) + 23
+  
+#Adjust parameter for black = .293, and cream = .279
+def cooling_law(temp,c):
+    return -c*(temp-23)
 
+def eulers_method(f,y0,a,b,dx):    
+    x = arange(a,b,dx)    
+    y = ones(size(x))*y0
+    for i in range(size(x)-1):
+        y[i+1] = y[i] + f(x[i],y[i])*dx
+    return (x,y)
 
+#USAGE
+# (t,mtblack) = eulers_method((lambda x,y: cooling_law(y,c_black)),82.3,0,46,.1)
