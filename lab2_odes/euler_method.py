b436c39b3c21562ff5301e324825c6429e4b6afb
Kevin Joyce <kevin.t.joyce@gmail.com>
2013-02-04 08:00:48 -0700

    Removed an uneeded file and added some base code to the eulers method class.

--- lab2_odes/euler_method.py
+++ lab2_odes/euler_method.py
@@ -2,19 +2,28 @@ from scipy import *
 from numpy import *
 from IPython.core.debugger import Tracer
 debug_here = Tracer()
-def numerical_integrate(f,y0,a,b,dt):    
-  t = arange(a,b,dt)    
+def numerical_integrate(method,f,y0,a,b,dt):
+  t = arange(a,b,dt)
   y = ones([size(t),size(y0)])
   y[0] = y[0]*y0 
   for i in range(size(t)-1):
-      y[i+1] = euler(y[i],f,dt)
+      y[i+1] = method(t,y[i],f,dt)
   return (t,y)
 
-def euler(y,f,dt):
-  return  #y + f(t,y)*dt
+def euler(t,y,f,dt):
+  return  y + f(t,y)*dt
 
-def newtons_second_law(t,y,F,m):
-  return array([ y[1], F/m ])
+# 1. Falling Object
+(a,b) = (0,1)
+def falling_object_system(deriv,m):
+  return array([ deriv[1], -9.8/m ])
 
-def analytic_solution(t):
-  return
+def numeric_falling_object(t,m):
+  return numerical_integrate(euler,
+
+def analytic_falling_object(t,m):
+  return #
+
+# 2. Harmonic Oscillator
+def oscilator_system(deriv,k):
+  
