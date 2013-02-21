3d74599cf38ff6e6097307a78ba984ee3b21db37
willrich <william1.richardson@umontana.edu>
2013-02-21 12:05:40 -0700

    Update N-body_OO_Fail.py

--- N-body_OO_Fail.py
+++ N-body_OO_Fail.py
@@ -14,27 +14,28 @@ class Body(object):
         self.m=m
         
     def state(self):
-        return array([x],[v],m)
+        return [x,v,m]
 
 class System(object):
     def __init__(self,bodylist):
         self.n=len(bodylist)
     def state(self,bodylist):
-        state=array([0 for col in range(2*n+n)])
+        state=[]
         for i in range(n):
-            state[2*i+1]=bodylist[i][0]     #position array x in first slot
-            state[2*(i+1)]=bodylist[i][1]   #velocity array v in second slot
-            state[-(n-i)]=bodylist[i][2]    #places masses at end of the list from m1,m2....mn
-        return state                        #e.g. x1,v1,x2,v2,x3,v3,m1,m2,m3
-                                            #Going deeper in: state=array([[x,y]1,[vx,vy]1,[x,y]2,
-                                                            #[vx,vy]2,[x,y]3,[vx,vy]3,m1,m2,m3])
-            
+            state.append(bodylist[i].state) 
+        state=array(state)
+        return state                        
+        
+        
+        
             
 class Force(object):
     def __init__(self,x1,x2,m1,m2):
         self.x1=x1
         self.x2=x2
-    G=6.67*10**(-11)
+        self.m1=m1
+        self.m2=m2
+        G=6.67*10**(-11)
     def component_forces(self,x1,x2,m1,m2):
         r=norm(x2-x1)
         xdist=norm(x2[0]-x1[0])
@@ -46,7 +47,7 @@ class Force(object):
 class Force_Pair(Force, System):
     def __init__(self):
         force_pairs=array([[0 for col in range(self.n)] for row in range(self.n)])
-    #    x=System.state
+    
         for i in range(0,self.n):
             x1=System.state[i][0]
             m1=System.state[i][2]
