a13ccc1306f1c87c3be3217b90e756b81bd16e81
Kevin Joyce <kevin.t.joyce@gmail.com>
2013-02-24 20:40:16 -0700

    Added the rest of the orbits.  They look "nice," but the Montgomery crashes.

--- lab6_oo_n_body/driver.py
+++ lab6_oo_n_body/driver.py
@@ -4,15 +4,14 @@ from scipy.integrate import odeint
 from numpy import *
 
 G = .5
-reader = Nbody.InitialConditions()
-sys_1 = reader.read_file('euler_init.txt')
-
-force = Nbody.Force(sys_1,G)
-
-t = linspace(0.,100.,1000)
-x = odeint(force,sys_1.init,t)
-print shape(x)
-plot(x[:,0],x[:,1])
-plot(x[:,2],x[:,3])
-plot(x[:,4],x[:,5])
-show()
+files = ('euler_init.txt','montgomery_init.txt','lagrange_init.txt')
+for fname in files:
+  reader = Nbody.InitialConditions()
+  sys_1 = reader.read_file(fname)
+  force = Nbody.Force(sys_1,G)
+  t = linspace(0.,10.,100)
+  x = odeint(force,sys_1.init,t)
+  plot(x[:,0],x[:,1])
+  plot(x[:,2],x[:,3])
+  plot(x[:,4],x[:,5])
+  show()
