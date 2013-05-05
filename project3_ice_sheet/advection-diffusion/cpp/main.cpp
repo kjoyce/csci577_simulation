202cd72e9274999dd2f38830b0f5691acd974755
Kevin Joyce <kevin.t.joyce@gmail.com>
2013-05-05 11:33:55 -0600

    ice sheet project

---
+++ project3_ice_sheet/advection-diffusion/cpp/main.cpp
@@ -0,0 +1,112 @@
+// Copyright (C) 2006-2008 Anders Logg
+//
+// This file is part of DOLFIN.
+//
+// DOLFIN is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DOLFIN is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+//
+// First added:  2006-02-09
+// Last changed: 2012-07-05
+//
+// This demo solves the time-dependent advection-diffusion equation
+// by a least-squares stabilized cG(1)cG(1) method. The velocity field
+// used in the simulation is the output from the Stokes (Taylor-Hood)
+// demo.  The sub domains for the different boundary conditions are
+// computed by the demo program in src/demo/subdomains.
+
+#include <dolfin.h>
+#include "AdvectionDiffusion.h"
+#include "Velocity.h"
+#include <dolfin/io/XMLFile.h>
+
+using namespace dolfin;
+
+int main(int argc, char *argv[])
+{
+  // Read mesh
+  Mesh mesh("../mesh.xml.gz");
+
+  // Create velocity FunctionSpace
+  Velocity::FunctionSpace V_u(mesh);
+
+  // Create velocity function
+  Function velocity(V_u);
+  XMLFile file_u("../velocity.xml.gz");
+  file_u >> velocity;
+
+  // Read sub domain markers
+  MeshFunction<std::size_t> sub_domains(mesh, "../subdomains.xml.gz");
+
+  // Create function space
+  AdvectionDiffusion::FunctionSpace V(mesh);
+
+  // Source term and initial condition
+  Constant f(0.0);
+  Function u(V);
+
+  // Set up forms
+  AdvectionDiffusion::BilinearForm a(V, V);
+  a.b = velocity;
+  AdvectionDiffusion::LinearForm L(V);
+  L.u0 = u; L.b = velocity; L.f = f;
+
+  // Set up boundary condition
+  Constant g(1.0);
+  DirichletBC bc(V, g, sub_domains, 1);
+
+  // Solution
+  Function u1(V);
+
+  // Linear system
+  boost::shared_ptr<Matrix> A(new Matrix);
+  Vector b;
+
+  // Assemble matrix
+  assemble(*A, a);
+  bc.apply(*A);
+
+  // LU solver
+  LUSolver lu(A);
+  lu.parameters["reuse_factorization"] = true;
+
+  // Parameters for time-stepping
+  const double T = 2.0;
+  const double k = 0.05;
+  double t = k;
+
+  // Output file
+  File file("results/temperature.pvd");
+
+  // Time-stepping
+  Progress p("Time-stepping");
+  while (t < T)
+  {
+    // Assemble vector and apply boundary conditions
+    assemble(b, L);
+    bc.apply(b);
+
+    // Solve the linear system (re-use the already factorized matrix A)
+    lu.solve(*u.vector(), b);
+
+    // Save solution in VTK format
+    file << std::make_pair(&u, t);
+
+    // Move to next interval
+    p = t / T;
+    t += k;
+  }
+
+  // Plot solution
+  plot(u);
+  interactive();
+}
---
+++ project3_ice_sheet/ice_sheet.py
@@ -0,0 +1,85 @@
+"""
+Simulation of Ice-Sheet temperature distribution
+""" 
+from dolfin import *
+import numpy as np
+import pylab as py
+
+spy = 31556926.
+# Parameters with meaningful change 
+zb = 300.
+v_ice_rate = -.2
+slope = .1E-5
+dth_dx = 2E-4
+qgeo = 30E-3
+dz = 20
+dt = spy
+
+# Constants 
+zs = 0.
+g = 9.81
+spy = 31556926.
+rho = 911.
+cp = 2009.
+beta = 9.8E-8
+thetapmp = beta*rho*g*(zs - zb)
+k = 2.1
+
+# Surface temperature
+def surface_temp(t):
+  return -10 + 5*np.sin(2*pi*t/spy)
+
+# Expressions
+#sigma = lambda z: (z-zb)/(zs-zb)
+#dsigma_dz = (zs-zb)**(-1)
+#u = lambda z: 20-100*sigma(z)**4
+#du_dz = lambda z: -400.*sigma(z)*dsigma_dz
+#w = lambda z: v_ice*sigma(z)**4
+sigma_exp = '(x[0] - zb)/(zs - zb)'
+sigma = Expression(sigma_exp,zb=zb,zs=zs)
+u = Expression('20-100*pow('+sigma_exp+',4)',zb=zb,zs=zs)
+w = Expression(('v_ice_rate*pow('+sigma_exp+',4)',),zb=zb,zs=zs,v_ice_rate=v_ice_rate)
+phi = lambda z: -rho*g*(zs - z)*du_dz(z)*slope
+phi = Expression('-rho*g*(zs - x[0])*-400*'+sigma_exp+'/(zs-zb)*slope',zb=zb,zs=zs,rho=rho,g=g,slope=slope)
+
+# Load Interval mesh
+mesh = IntervalMesh(dz,zs,zb) 
+
+# Create Solution Space
+Q = FunctionSpace(mesh, "CG", 1)
+
+# Define test and trial functions
+theta, v = TrialFunction(Q), TestFunction(Q)
+
+# Define last solution based on surface temperature
+theta_last = interpolate(Expression("T0",T0=surface_temp(0)),Q)
+
+# Variational Problem
+a = (theta*v + dt*k/rho*cp*spy*dot(nabla_grad(theta),nabla_grad(v)) + spy*dt*dot(w,nabla_grad(theta))*v*dt)*dx
+L = (theta_last*v - dt*u*dth_dx*v + phi*v/rho/cp*dt)*dx + (qgeo*v*dt/rho/cp)*ds
+
+# Boundary Conditions 
+def DirichletBoundary(x, on_boundary):
+  tol = 1E-14
+  return on_boundary and (abs(x[0]) < tol)
+
+theta0 = Expression('T0',T0=surface_temp(0))
+bc = DirichletBC(Q, theta0, DirichletBoundary)
+
+A = assemble(a)
+b = None
+
+theta = Function(Q)
+total_time = 1000*spy
+t = dt
+while t <= total_time:
+  b = assemble(L)
+  theta0.t = t
+  bc.apply(A,b)
+  solve(A,theta.vector(), b)
+  t += dt
+  theta_last.assign(theta)
+
+py.plot(theta.vector().array(),mesh.coordinates())
+
+
